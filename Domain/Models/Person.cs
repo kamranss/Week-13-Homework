b82e04455eef900f5a1d4f1b36d7d740481425d1
kamranss <kamranss@code.edu.az>
2022-12-17 16:28:44 +0400

    Refactor: Project updated

--- Domain/Models/Person.cs
+++ Domain/Models/Person.cs
@@ -8,9 +8,9 @@ namespace Domain.Models
 {
     public class Person
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Address { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Address { get; set; }
         public int Salary { get; set; }
     }
 }
